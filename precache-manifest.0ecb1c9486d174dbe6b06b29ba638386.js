b942def0a8c82b28a417e608ca4af1f997003da1
Rjhj21 <92937892+RonnieJr21@users.noreply.github.com>
2023-01-26 16:20:15 -0500

    Updates

---
+++ precache-manifest.0ecb1c9486d174dbe6b06b29ba638386.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "341964d03ed1bebaa4d8",
+    "url": "/Web-Ronnie/static/css/main.2dbf1b4b.chunk.css"
+  },
+  {
+    "revision": "341964d03ed1bebaa4d8",
+    "url": "/Web-Ronnie/static/js/main.341964d0.chunk.js"
+  },
+  {
+    "revision": "4c92f7fe64aff0f606b3",
+    "url": "/Web-Ronnie/static/js/1.4c92f7fe.chunk.js"
+  },
+  {
+    "revision": "d0a63bcb74b2289cb1f6",
+    "url": "/Web-Ronnie/static/js/runtime~main.d0a63bcb.js"
+  },
+  {
+    "revision": "a1b58e9cd986bcf92ce62c791434c2c1",
+    "url": "/Web-Ronnie/static/media/cartoonSpaceship.a1b58e9c.png"
+  },
+  {
+    "revision": "79eb960daa30282af1fad31b33b39255",
+    "url": "/Web-Ronnie/static/media/astronaut-on-moon.79eb960d.png"
+  },
+  {
+    "revision": "587e43af987201b9ce130232912e75c0",
+    "url": "/Web-Ronnie/index.html"
+  }
+];
