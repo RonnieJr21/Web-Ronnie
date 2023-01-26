872c03558983299bf34448aa19b33089346a99f1
Rjhj21 <92937892+RonnieJr21@users.noreply.github.com>
2023-01-26 16:53:57 -0500

    i think it is working now TAKE 2

--- src/Components/navbar/navbar.js
+++ src/Components/navbar/navbar.js
@@ -4,11 +4,11 @@ import './navbar.css'
 const MyNavbar = () => {
     return(
         <div className="navbar">
-            <span><a className="Home" href={"http://localhost:3000/"} > Home Page </a></span>
+            <span><a className="Home" href={process.env.PUBLIC_URL + "/"} > Home Page </a></span>
 
-            <span><a className="Projects" href={"/projects"} > Projects </a></span>
+            <span><a className="Projects" href={process.env.PUBLIC_URL + "/projects"} > Projects </a></span>
 
-            <span><a className="AboutMe" href="http://localhost:3000/about-me" > About me </a></span>
+            <span><a className="AboutMe" href={process.env.PUBLIC_URL + "/about-me"} > About me </a></span>
 
             <span><a className="Skills" href="./Skills/Skills.html"> Skills </a></span>
 
